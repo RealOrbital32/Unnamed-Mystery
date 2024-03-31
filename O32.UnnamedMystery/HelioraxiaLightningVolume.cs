2fffa09d835fae3b284e2d12ca2473c0f7dcba6c
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-03-31 00:08:26 -0400

    Add helioraxia lightning volume

---
+++ O32.UnnamedMystery/HelioraxiaLightningVolume.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnnamedMystery
+{
+    [RequireComponent(typeof(OWTriggerVolume), typeof(OWAudioSource))]
+    public class HelioraxiaLightningVolume : HazardVolume
+    {
+        /// <summary>
+        /// Speed relative to the planet that will make the lightning hit you
+        /// </summary>
+        public float zapSpeed;
+
+        /// <summary>
+        /// Cooldown between each shock. Don't make this too low or else it will just keep shocking lol.
+        /// </summary>
+        private static float _shockRepeatTime = 5;
+
+        private float _flashbangFadeLength = 1;
+
+        private float _shipElectroKnockoutTime;
+        private bool _isFlashbanged;
+        private float _flashbangStartTime;
+        private OWAudioSource _audioSource;
+        private List<TrackedHazardDetector> _trackedDetectors;
+
+        public override void Awake()
+        {
+            base.Awake();
+            _trackedDetectors = new List<TrackedHazardDetector>(4);
+            _audioSource = this.GetRequiredComponent<OWAudioSource>();
+            _firstContactDamageType = InstantDamageType.Electrical;
+        }
+
+        private void LateUpdate()
+        {
+            foreach (var tracked in _trackedDetectors)
+            {
+                bool insulated = tracked.hazardDetector.IsInsulated();
+                if (!insulated && (tracked.wasInsulated || tracked.CanShock()))
+                {
+                    OWRigidbody attachedOWRigidbody = tracked.hazardDetector.GetAttachedOWRigidbody();
+                    if (attachedOWRigidbody == null) continue;
+                    var speed = attachedOWRigidbody.GetRelativeVelocity(this.GetAttachedOWRigidbody()).magnitude;
+                    if (speed > zapSpeed)
+                    {
+                        ApplyShock(tracked.hazardDetector);
+                        tracked.lastShockTime = Time.timeSinceLevelLoad;
+                    }
+                }
+                tracked.wasInsulated = insulated;
+            }
+
+            if (_isFlashbanged)
+            {
+                UpdateFlashbang();
+            }
+        }
+
+        private void ApplyShock(HazardDetector detector)
+        {
+            detector.PlayElectricityEffects();
+            PlayLightningSoundEffect();
+            OWRigidbody attachedOWRigidbody = detector.GetAttachedOWRigidbody();
+            if (attachedOWRigidbody == null) return;
+
+            Vector3 normalized = (detector.transform.position - transform.position).normalized;
+            if (_triggerVolume.GetPenetrationDistance(detector.transform.position) < 1)
+            {
+                Vector3 vector = attachedOWRigidbody.GetVelocity() - _attachedBody.GetPointVelocity(detector.transform.position);
+                attachedOWRigidbody.AddVelocityChange(-Vector3.Project(vector, normalized));
+                attachedOWRigidbody.AddVelocityChange(normalized * 10);
+            }
+
+            if (detector.CompareTag("PlayerDetector"))
+            {
+                RumbleManager.Fade(0.8f, 0.2f, 2.5f);
+                StartFlashbang();
+                detector.GetAttachedOWRigidbody().GetComponent<PlayerResources>().ApplyInstantDamage(_firstContactDamage, _firstContactDamageType);
+            }
+            else
+            {
+                attachedOWRigidbody.AddAngularVelocityChange(UnityEngine.Random.onUnitSphere * 10f);
+            }
+
+            if (detector.CompareTag("ShipDetector"))
+            {
+                ShipDamageController shipDamageController = attachedOWRigidbody.GetComponent<ShipDamageController>();
+                if (shipDamageController.IsElectricalFailed())
+                {
+                    if (Time.timeSinceLevelLoad > _shipElectroKnockoutTime + 3f)
+                    {
+                        shipDamageController.TriggerReactorCritical();
+                    }
+                }
+                else
+                {
+                    shipDamageController.TriggerElectricalFailure();
+                    _shipElectroKnockoutTime = Time.timeSinceLevelLoad;
+                }
+                attachedOWRigidbody.GetComponentInChildren<ShipCockpitController>().LockUpControls(3f);
+            }
+        }
+
+        private void StartFlashbang()
+        {
+            _flashbangStartTime = Time.time;
+            _isFlashbanged = true;
+        }
+
+        private void UpdateFlashbang()
+        {
+            float lerp = Mathf.InverseLerp(_flashbangStartTime, _flashbangStartTime + _flashbangFadeLength, Time.time);
+            if (lerp < 1)
+            {
+                var camera = Locator.GetPlayerCamera();
+                camera.postProcessingSettings.colorGrading.postExposure = Mathf.Lerp(camera.postProcessingSettings.colorGradingDefault.postExposure, 20, lerp);
+            }
+            else
+            {
+                StopFlashbang();
+            }
+        }
+
+        private void StopFlashbang()
+        {
+            _isFlashbanged = false;
+            Locator.GetPlayerCamera().postProcessingSettings.colorGrading.postExposure = 0;
+        }
+
+        private void PlayLightningSoundEffect()
+        {
+            _audioSource.PlayOneShot(UnnamedMystery.HelioraxiaThunder);
+        }
+
+        public override void OnEffectVolumeEnter(GameObject hitObj)
+        {
+            HazardDetector detector = hitObj.GetComponent<HazardDetector>();
+            if (detector != null)
+            {
+                detector.AddVolume(this);
+                var insulated = detector.IsInsulated();
+                if (!_trackedDetectors.Exists(tracked => tracked.hazardDetector == detector))
+                {
+                    TrackedHazardDetector trackedHazardDetector = new TrackedHazardDetector();
+                    trackedHazardDetector.hazardDetector = detector;
+                    trackedHazardDetector.lastShockTime = 0;
+                    trackedHazardDetector.wasInsulated = insulated;
+                    _trackedDetectors.Add(trackedHazardDetector);
+                }
+            }
+        }
+
+        public override void OnEffectVolumeExit(GameObject hitObj)
+        {
+            base.OnEffectVolumeExit(hitObj);
+            HazardDetector detector = hitObj.GetComponent<HazardDetector>();
+            _trackedDetectors.RemoveAll((TrackedHazardDetector x) => x.hazardDetector == detector);
+        }
+
+        public override HazardType GetHazardType()
+        {
+            return HazardType.ELECTRICITY;
+        }
+
+        /// <summary>
+        /// Tracked hazard object for when a detector enters.
+        /// </summary>
+        private class TrackedHazardDetector
+        {
+            public HazardDetector hazardDetector;
+
+            public float lastShockTime;
+
+            public bool wasInsulated;
+
+            /// <summary>
+            /// Make sure it doesn't shock a morbillion times a second.
+            /// </summary>
+            /// <returns>Can we shock?</returns>
+            public bool CanShock()
+            {
+                return Time.timeSinceLevelLoad > lastShockTime + _shockRepeatTime;
+            }
+        }
+    }
+}
--- O32.UnnamedMystery/UnnamedMystery.cs
+++ O32.UnnamedMystery/UnnamedMystery.cs
@@ -12,11 +12,70 @@ namespace UnnamedMystery
     {
         public INewHorizons _newHorizons;
 
+        public static AudioType HelioraxiaThunder;
+
         private void Start()
         {
             //Base stuff that actually loads the configs
             _newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
             _newHorizons.LoadConfigs(this);
+
+            // Extra stuff to detect when planets get loaded
+            _newHorizons.GetBodyLoadedEvent().AddListener((string name) =>
+            {
+                // check if this is unnamed mystery system
+                if (_newHorizons.GetCurrentStarSystem() == "O32.UnnamedMystery")
+                {
+                    // check if helioraxia
+                    if (name == "Helioraxia") OnHelioraxiaLoaded(_newHorizons.GetPlanet("Helioraxia"));
+                }
+            });
+
+            // Extra stuff to detect when planets get loaded
+            _newHorizons.GetStarSystemLoadedEvent().AddListener((string name) =>
+            {
+                // check if this is unnamed mystery system
+                if (name == "O32.UnnamedMystery")
+                {
+                    // Load lightning audio clips for helioraxia
+                    var thunderA = AudioUtilities.LoadAudio(Path.Combine(ModHelper.Manifest.ModFolderPath, "planets/oneshot/thunderA.mp3"));
+                    var thunderB = AudioUtilities.LoadAudio(Path.Combine(ModHelper.Manifest.ModFolderPath, "planets/oneshot/thunderB.mp3"));
+                    HelioraxiaThunder = AudioTypeHandler.AddCustomAudioType("HelioraxiaThunder", new AudioClip[] { thunderA, thunderB });
+                }
+            });
+        }
+
+        public void OnHelioraxiaLoaded(GameObject helioraxia)
+        {
+            ModHelper.Console.WriteLine("Helioraxia has loaded", MessageType.Info);
+
+            //Get helioraxia sector
+            Sector sector = helioraxia.transform.Find("Sector").GetComponent<Sector>();
+
+            //Add new game object to the sector of helioraxia
+            GameObject zapperObject = new GameObject("ElectricalSystemZapper");
+            zapperObject.SetActive(false);
+            zapperObject.layer = Layer.BasicEffectVolume;
+            zapperObject.transform.SetParent(sector.transform, false);
+
+            //Add sphere shape volume to detect when player or something enters.
+            SphereShape sphere = zapperObject.AddComponent<SphereShape>();
+            sphere._collisionMode = Shape.CollisionMode.Volume;
+            sphere._pointChecksOnly = true;
+            sphere._radius = _newHorizons.QueryBody<float>("Helioraxia", "extras.helioraxiaZapper.radius");
+            zapperObject.AddComponent<OWTriggerVolume>()._shape = sphere;
+
+            //Add audio source for thunder
+            zapperObject.AddComponent<AudioSource>();
+            zapperObject.AddComponent<OWAudioSource>();
+
+            //Finally the component to handle the actual stuff
+            HelioraxiaLightningVolume zapper = zapperObject.AddComponent<HelioraxiaLightningVolume>();
+            zapper.zapSpeed = _newHorizons.QueryBody<float>("Helioraxia", "extras.helioraxiaZapper.zapSpeed");
+            zapper._attachedBody = helioraxia.GetAttachedOWRigidbody();
+            zapper._firstContactDamage = _newHorizons.QueryBody<float>("Helioraxia", "extras.helioraxiaZapper.damage");
+
+            zapperObject.SetActive(true);
         }
     }
 

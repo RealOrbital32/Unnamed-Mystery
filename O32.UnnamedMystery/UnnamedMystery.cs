d265edfe53e2a1c96d2b8f0425c04137b979583d
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-03-31 00:06:26 -0400

    Add code project

---
+++ O32.UnnamedMystery/UnnamedMystery.cs
@@ -0,0 +1,23 @@
+﻿using NewHorizons.Handlers;
+using NewHorizons.Utility.Files;
+using NewHorizons.Utility.OuterWilds;
+using OWML.Common;
+using OWML.ModHelper;
+using System.IO;
+using UnityEngine;
+
+namespace UnnamedMystery
+{
+    public class UnnamedMystery : ModBehaviour
+    {
+        public INewHorizons _newHorizons;
+
+        private void Start()
+        {
+            //Base stuff that actually loads the configs
+            _newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            _newHorizons.LoadConfigs(this);
+        }
+    }
+
+}
